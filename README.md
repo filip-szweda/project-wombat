72e97f3033f39a86c69cb3d1ac1e24a89c0d4e9c
FilipSzweda <filip.szweda1@gmail.com>
2023-03-17 15:23:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# project_wombat
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/project_wombat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_wombat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
